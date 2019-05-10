c08e0a31d5a99896d40c8f89fe1755f05181aec1
fadingbeat <fadingbeat@gmail.com>
2019-05-10 08:40:27 +0200

    Created header component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from './navbar';
-
+import App from './App';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
-
-
-
-ReactDOM.render(<NavBar />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
